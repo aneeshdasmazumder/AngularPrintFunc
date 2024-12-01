c054e94ff43e2ba415cd14bd8087bcccf354b336
aneeshdasmazumder <aneeshdasmazumder@gmail.com>
2024-12-01 23:56:08 +0530

    Angular Print

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'adm-todo-list';
+
+  printPage() {
+    // Grab the content to print
+    const printContents = document.getElementById('contentToPrint')?.innerHTML;
+
+    // Create a new window for printing
+    const printWindow = window.open('', '', 'height=600,width=800');
+
+    // Write the content to the new window
+    printWindow?.document.write('<html><head><title>Print</title>');
+    printWindow?.document.write('</head><body>');
+    printWindow?.document.write(printContents || ''); // if content exists, insert it
+    printWindow?.document.write('</body></html>');
+
+    // Close the document and trigger the print dialog
+    printWindow?.document.close();
+    printWindow?.focus();
+    printWindow?.print();
+    printWindow?.close();
+  }
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.title = "Changed Title";
+  //   }, 5000);
+  // }
 }
